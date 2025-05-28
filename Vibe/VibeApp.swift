3c0487c262c2344ece66000b515ae5dac3d348ed
aung <hlumaungphyo@gmail.com>
2025-05-28 09:22:51 +0900

    Initial Commit

---
+++ Vibe/VibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeApp.swift
+//  Vibe
+//
+//  Created by cmStudent on 2025/05/28.
+//
+
+import SwiftUI
+
+@main
+struct VibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
