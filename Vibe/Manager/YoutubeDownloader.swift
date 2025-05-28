f21be1302c2f7305a73c2481b550ee2efd1a229d
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-05-28 10:33:25 +0900

    created YoutubDownloader to download audio from youtube urls

---
+++ Vibe/Manager/YoutubeDownloader.swift
@@ -0,0 +1,102 @@
+//
+//  YoutubeDownloader.swift
+//  Vibe
+//
+//  Created by cmStudent on 2025/05/28.
+//
+
+import Foundation
+import YouTubeKit
+
+
+enum YoutubeDownloaderError: LocalizedError {
+    case invalidURL
+    case invalidResponse
+    case streamNotFound
+    case downloadableURLNotFound
+    case decodingError(Error)
+    case networkError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return NSLocalizedString("Invalid URL", comment: "")
+        case .downloadableURLNotFound:
+            return NSLocalizedString("Downloadable URL not found.", comment: "")
+        case .streamNotFound:
+            return NSLocalizedString("Stream not found.", comment: "")
+        case .invalidResponse:
+            return NSLocalizedString("Invalid response from server", comment: "")
+        case .decodingError(let error):
+            return String(format: NSLocalizedString("Failed to decode response: %@", comment: ""), error.localizedDescription)
+        case .networkError(let error):
+            return String(format: NSLocalizedString("Network error: %@", comment: ""), error.localizedDescription)
+        }
+    }
+}
+
+
+final class YoutubeDownloader {
+    let progressHandler: ((Double) -> Void)? = nil
+    private let fileDownloader: FileDownloader = FileDownloader()
+    
+    
+    func downloadAudioAndSave(from urlString: String, fileName: String) async throws -> URL {
+        guard let url = URL(string: urlString) else {
+            throw YoutubeDownloaderError.invalidURL
+        }
+        progressHandler?(0)
+        guard let downloadableURL = try await getDownloadableURL(from: url) else {
+            throw YoutubeDownloaderError.downloadableURLNotFound
+        }
+        
+        let downloadedURL = try await downloadFile(from: downloadableURL)
+        let data = try Data(contentsOf: downloadedURL)
+
+        return try saveFile(with: data, fileName: fileName)
+    }
+    
+    private func getDownloadableURL(from url: URL) async throws -> URL? {
+        
+      
+        let video = YouTube(url: url)
+        let streams = try await video.streams
+        let audioOnlyStreams = streams.filterAudioOnly()
+        
+        guard let stream = audioOnlyStreams.filter ({ $0.isNativelyPlayable }).highestAudioBitrateStream() else {
+            throw YoutubeDownloaderError.streamNotFound
+        }
+        
+        return stream.url
+    }
+    
+    
+    private func downloadFile(from url: URL) async throws -> URL {
+        return try await withCheckedThrowingContinuation { continuation in
+            fileDownloader.download(
+                from: url) { progress in
+                    self.progressHandler?(progress)
+                } completionHandler: { result in
+                    switch result {
+                    case .success(let fileURL):
+                        continuation.resume(returning: fileURL)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
+                }
+        }
+    }
+    
+    private func saveFile(with data: Data, fileName: String) throws -> URL {
+        let documentsPath = FileManager.default.urls(for: .musicDirectory, in: .userDomainMask)[0]
+        let localURL = documentsPath.appendingPathComponent("\(fileName).m4a")
+        
+        do {
+            try data.write(to: localURL)
+            return localURL
+        } catch {
+            throw YoutubeDownloaderError.networkError(error)
+        }
+    }
+    
+}
