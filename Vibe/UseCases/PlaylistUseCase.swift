641013ed19b79c83ce8e0ab3d4b1b6568c8a6fe3
aung <hlumaungphyo@gmail.com>
2025-06-02 09:59:23 +0900

    Create PlaylistUseCase

--- Vibe/UseCases/PlaylistUseCase.swift
+++ Vibe/UseCases/PlaylistUseCase.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class PlaylistUseCase {
+    private let playlistRepository: PlaylistRepository
+    
+    init(playlistRepository: PlaylistRepository) {
+        self.playlistRepository = playlistRepository
+    }
+    
+    
+    func addPlaylist(_ playlist: Playlist) async {
+        do {
+            try await playlistRepository.addPlaylist(playlist)
+        } catch {
+            print("Error adding playlist: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func getAllPlaylists() async -> [Playlist] {
+        do {
+            return try await playlistRepository.getAllPlaylists()
+        } catch {
+            print("Error getting playlists: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    
+    func deletePlaylist(_ playlist: Playlist) async {
+        do {
+            try await playlistRepository.deletedPlaylist(playlist)
+        } catch {
+            print("Error deleting playlist: \(error.localizedDescription)")
+        }
+    }
+}
