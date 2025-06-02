5b42c8a434cbf4ff2165d6f0aa2774eb17c8580c
aung <hlumaungphyo@gmail.com>
2025-06-02 13:32:56 +0900

    feat: add SwiftDataAudioRepoImpl for audio repository implementation

---
+++ Vibe/Domain/Data/SwiftDataAudioRepoImpl.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftDataManager.swift
+//  Vibe
+//
+//  Created by cmStudent on 2025/05/28.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class SwiftDataAudioRepoImpl : AudioDataRepository {
+    let context: ModelContext
+    
+    init(context: ModelContext) {
+        self.context = context
+    }
+    
+    func save(_ downloadedAudio: DownloadedAudio) throws {
+        context.insert(downloadedAudio)
+        try context.save()
+    }
+    
+    func fetchAllDownloadedAudio() throws -> [DownloadedAudio] {
+        let descriptor = FetchDescriptor<DownloadedAudio>()
+        return try context.fetch(descriptor)
+    }
+    
+    
+    func deleteDownloadedAudio(_ downloadedAudio: DownloadedAudio) throws {
+        context.delete(downloadedAudio)
+        try context.save()
+    }
+    
+    
+    func addToPlaylist(_ audio: DownloadedAudio, to playlist: Playlist) throws {
+        audio.playlist.append(playlist)
+        try context.save()
+    }
+}
