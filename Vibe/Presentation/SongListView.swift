9e71c767a94a96be0b253c865b7110fcf4e8be99
aung <hlumaungphyo@gmail.com>
2025-06-03 10:42:06 +0900

    feat: Update UI components to support playlist functionality

--- Vibe/Presentation/SongListView.swift
+++ Vibe/Presentation/SongListView.swift
@@ -15,7 +15,8 @@ final class SongListViewModel: ObservableObject {
     @Published var currentAudio: DownloadedAudio?
     @Published var isLooping: Bool = false
     @Published var playlists: [Playlist] = []
-
+    @Published var songsInPlaylist: [DownloadedAudio] = []
+    var playlistType: PlaylistType
     
     let savedAudioUseCase: SavedAudioUseCase
     let audioPlayerUseCase: AudioPlayerUseCase
@@ -24,15 +25,34 @@ final class SongListViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     
     init(
+        playlistType: PlaylistType = .all,
         savedAudioUseCase: SavedAudioUseCase,
         audioPlayerUseCase: AudioPlayerUseCase,
         playlistUseCase: PlaylistUseCase
     ) {
+        self.playlistType = playlistType
+        
         self.savedAudioUseCase = savedAudioUseCase
         self.audioPlayerUseCase = audioPlayerUseCase
         self.playlistUseCase = playlistUseCase
-        
+
         setupBindings()
+        Task {
+            await getPlaylists()
+            await loadSongsFromPlaylist()
+        }
+
+    }
+    
+    
+    func loadSongsFromPlaylist() async {
+        do {
+            print(playlistType.displayName + " playlist loaded")
+            self.songsInPlaylist = try await savedAudioUseCase.getSavedAudios(playlistType: self.playlistType)
+            print(songsInPlaylist.count)
+        } catch {
+            print("Error loading songs from playlist: \(error.localizedDescription)")
+        }
     }
     
     private func setupBindings() {
@@ -55,6 +75,8 @@ final class SongListViewModel: ObservableObject {
     }
     
     func playAudio(_ audio: DownloadedAudio) {
+        audioPlayerUseCase.updateCurrentPlaylistSongs(playlistType: playlistType)
+        
         if currentAudio?.id == audio.id {
             if isPlaying {
                 pauseAudio()
@@ -94,7 +116,7 @@ final class SongListViewModel: ObservableObject {
                 audioPlayerUseCase.stop()
             }
             try await savedAudioUseCase.deleteAudio(audio)
-            audioPlayerUseCase.updatePlaylist()
+            audioPlayerUseCase.updateAllSongsList()
         } catch {
             print("Error deleting audio: \(error.localizedDescription)")
         }
@@ -123,22 +145,21 @@ extension SongListViewModel {
 }
 
 struct SongListView: View {
-    var songs: [DownloadedAudio]
 
-    @State private var selectedSongToAddToPlaylist: DownloadedAudio?
+    @State private var selectedSongToAddToPlaylist: DownloadedAudio? = nil
     @StateObject private var vm: SongListViewModel
     
     init(
-        songs: [DownloadedAudio],
+        playListType: PlaylistType,
         selectedSongToAddToPlaylist: DownloadedAudio? = nil,
         savedAudioUseCase: SavedAudioUseCase,
         audioPlayerUseCase: AudioPlayerUseCase,
         playlistUseCase: PlaylistUseCase
     ) {
-        self.songs = songs
         self.selectedSongToAddToPlaylist = selectedSongToAddToPlaylist
         _vm = .init(
             wrappedValue: .init(
+                playlistType: playListType,
                 savedAudioUseCase: savedAudioUseCase,
                 audioPlayerUseCase: audioPlayerUseCase,
                 playlistUseCase: playlistUseCase
@@ -148,17 +169,18 @@ struct SongListView: View {
     
     var body: some View {
         List {
-            ForEach(songs) { audio in
+            ForEach(vm.songsInPlaylist) { audio in
                 Button {
-                    if vm.currentAudio?.id == audio.id {
-                        if vm.isPlaying {
-                            vm.pauseAudio()
-                        } else {
-                            vm.resumeAudio()
-                        }
-                    } else {
-                        vm.playAudio(audio)
-                    }
+                    vm.playAudio(audio)
+//                    if vm.currentAudio?.id == audio.id {
+//                        if vm.isPlaying {
+//                            vm.pauseAudio()
+//                        } else {
+//                            vm.resumeAudio()
+//                        }
+//                    } else {
+//                        vm.playAudio(audio)
+//                    }
                     
                 } label: {
                     AudioItemRow(
@@ -189,56 +211,60 @@ struct SongListView: View {
                 }
             }
             .sheet(
-                item: $selectedSongToAddToPlaylist,
-                content: { song in
-                    NavigationStack {
-                        List {
-                            ForEach(vm.playlists) { playlist in
-                                Button {
-                                    Task {
-                                        await vm.addSongToPlaylist(song, playlist)
-                                        selectedSongToAddToPlaylist = nil
-                                    }
-                                }label: {
-                                    PlaylistItemView(
-                                        playlist: playlist,
-                                        isNavigationLinkActive: false,
-                                        savedAudioUseCase: vm.savedAudioUseCase,
-                                        audioPlayerUseCase: vm.audioPlayerUseCase,
-                                        playlistUseCase: vm.playlistUseCase
-                                    )
-                                }
-                                .buttonStyle(.plain)
-                                
-                            }
-                            
-                        }
-                        .listStyle(.plain)
-                        .navigationTitle("Add to playlist")
-                        
-                    }
-                    
-                Spacer()
-                
-            })
+                item: $selectedSongToAddToPlaylist,content: { song in
+                    playListSelectionSheet(song: song)
+                })
         }
         .listStyle(.plain)
                     
                     
-        .task {
-            await vm.getPlaylists()
-        }
+        
+        
         
 
     }
 }
 
 
+extension SongListView {
+    
+    @ViewBuilder
+    private func playListSelectionSheet(song: DownloadedAudio) -> some View {
+        NavigationStack {
+            List {
+                ForEach(vm.playlists) { playlist in
+                    Button {
+                        Task {
+                            await vm.addSongToPlaylist(song, playlist)
+                            selectedSongToAddToPlaylist = nil
+                        }
+                    }label: {
+                        PlaylistItemView(
+                            playlistType: .playlist(playlist),
+                            isNavigationLinkActive: false,
+                            savedAudioUseCase: vm.savedAudioUseCase,
+                            audioPlayerUseCase: vm.audioPlayerUseCase,
+                            playlistUseCase: vm.playlistUseCase
+                        )
+                    }
+                    .buttonStyle(.plain)
+                    
+                }
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("Add to playlist")
+            
+        }
+        Spacer()
+    }
+}
+
 #Preview {
     @Previewable
     @Environment(\.container) var container
     SongListView(
-        songs: [],
+        playListType: .all,
         savedAudioUseCase: container.savedAudioUseCase,
         audioPlayerUseCase: container.audioPlayerUseCase,
         playlistUseCase: container.playlistUseCase
