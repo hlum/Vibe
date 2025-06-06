6714ccdd156f0ff5b365754ec08b93099e5f6a93
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2025-06-06 09:47:55 +0900

    add imgURL property to the model

--- Vibe/Entity/DownloadedAudio.swift
+++ Vibe/Entity/DownloadedAudio.swift
@@ -14,13 +14,15 @@ final class DownloadedAudio {
     var title: String
     @Relationship(inverse: \Playlist.songs) var playlist: [Playlist] = []
     var originalURL: String
+    var imgURL: String?
     var downloadDate: Date
     var duration: TimeInterval
     
-    init(title: String, originalURL: String, duration: TimeInterval = 0, playlist: [Playlist] = []) {
+    init(title: String, originalURL: String,imgURL: String? = nil, duration: TimeInterval = 0, playlist: [Playlist] = []) {
         self.id = UUID().uuidString
         self.title = title
         self.originalURL = originalURL
+        self.imgURL = imgURL
         self.downloadDate = Date()
         self.duration = duration
         self.playlist = playlist
